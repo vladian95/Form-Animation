3d691c5e0dc5ace411fc28b4684ec5d3eca3fa4d
vladian95 <kvankvan1995@gmail.com>
2022-03-29 17:24:11 +0300

    start proj

---
+++ app.js
@@ -0,0 +1,10 @@
+const containers = document.querySelectorAll('.input-container');
+const form = document.querySelector('form');
+
+const tl = gsap.timeline({defaults: {duration: 1} });
+
+// line
+const start = 
+"M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512";
+const end = 
+
