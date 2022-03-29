82f6396f236884c76c9cc89969891d39776772e0
vladian95 <kvankvan1995@gmail.com>
2022-03-29 18:05:06 +0300

    placeholder jump

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ containers.forEach(container => {
                 { attr: { d: end }, ease: 'Power2.easeOut', duration: 0.75 }
                 );
                tl.to(line, {attr: {d: start}, ease: 'elastic.out(3,0.5)'}, '<50%');
+               //Placeholder Shift
+               tl.to(placeholder, {
+                top: -15,
+                left: 0,
+                scale: 0.7,
+                duration: 0.5,
+                ease: "Power2.easeOut",
+               }, "<15%");
             }
         });
 });
