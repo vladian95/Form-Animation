efef7d01985e0478edd849332a9788b55d94990c
vladian95 <kvankvan1995@gmail.com>
2022-03-29 17:53:17 +0300

    added elastic effect

--- app.js
+++ app.js
@@ -6,5 +6,24 @@ const tl = gsap.timeline({defaults: {duration: 1} });
 // line
 const start = 
 "M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512";
-const end = 
 
+const end = 
+"M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512";
+
+// Elastic Effect
+containers.forEach(container => {
+    const input = container.querySelector('.input');
+    const line = container.querySelector('.elastic-line');
+    const placeholder = container.querySelector('.placeholder');
+
+        input.addEventListener('focus', () => {
+            //Check to see if there is any text in the input
+            if(!input.value){
+               tl.fromTo(line, 
+                { attr: { d: start } }, 
+                { attr: { d: end }, ease: 'Power2.easeOut', duration: 0.75 }
+                );
+               tl.to(line, {attr: {d: start}, ease: 'elastic.out(3,0.5)'}, '<50%');
+            }
+        });
+});
