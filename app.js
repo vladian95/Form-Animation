c5bfe2de875ba423f5a94854f806e2a8e673658b
vladian95 <kvankvan1995@gmail.com>
2022-03-30 20:45:19 +0300

    added Validation Checks

--- app.js
+++ app.js
@@ -49,6 +49,56 @@ form.addEventListener('click', () => {
                 gsap.to(placeholder, {top: 0, left: 0, scale: 1, duration: 0.5, ease:"Power2.easeOut"})
             }
         }
-    })
+        // We will do our validation
+        //Name Validation
+        input.addEventListener('input', (e) => {
+            if(e.target.type === 'text'){
+                let inputText = e.target.value;
+                if(inputText.length > 2){
+                    colorize('#6391E8', line, placeholder);
+                }else{
+                    colorize('#FE8C99', line, placeholder);
+                }
+            }
+            //Email validation
+            if(e.target.type === 'email'){
+                let valid = validateEmail(e.target.value);
+                if(valid){
+                    colorize('#6391E8', line, placeholder);
+                }else{
+                    colorize('#FE8C99', line, placeholder);
+                }
+            }
+            //Validate phone
+            if(e.target.type === 'tel'){
+                let valid = validatePhone(e.target.value);
+                if(valid){
+                    colorize('#6391E8', line, placeholder);
+                }else{
+                    colorize('#FE8C99', line, placeholder);
+                }
+            }
+        });
+    });
 });
+
+// checking email validation
+
+function validateEmail(email) {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+function validatePhone(phone) {
+    let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return re.test(phone);
+}
+
+
+//COLORIZE FUNCTION
+function colorize(color, line, placeholder){
+    gsap.to(line, {stroke: color, duration: 0.75});
+    gsap.to(placeholder,{color: color, duration: 0.75});
+}
+
+
 
